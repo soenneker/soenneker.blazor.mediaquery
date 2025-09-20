e87608aa92f9a294abb8db3a5e885d8863169ec9
Jake Soenneker <jake@soenneker.com>
2025-09-20 17:32:08 -0500

    -> Core

--- src/Abstract/IMediaQuery.cs
+++ src/Abstract/IMediaQuery.cs
@@ -1,10 +1,10 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Soenneker.Quark.Components.Core.Cancellable.Abstract;
 
 namespace Soenneker.Blazor.MediaQuery.Abstract;
 
-public interface IMediaQuery : ICancellableElement
+public interface IMediaQuery : ICoreCancellableElement
 {
     /// <summary>
     /// Asynchronously checks if the specified media query matches the current viewport.
